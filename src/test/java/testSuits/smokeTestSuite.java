8ce445b4db95464986e37c1b98de9dbab8b5cf96
an.vovk <wowkaiwork@gmail.com>
2018-01-14 21:27:53 +0200

    maven dependency files

--- src/test/java/testSuits/smokeTestSuite.java
+++ src/test/java/testSuits/smokeTestSuite.java
@@ -1,17 +1,17 @@
 package testSuits;
 
-/**
- * This is test suit with all tests which are needed for smoke test
- */
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import pt_Global_Header_Tests.BulkOrderHeader_Tests;
+import pt_Global_Header_Tests.Find_Equipment_Manuals_Search_Tests;
 import pt_Global_Header_Tests.Global_Search_Tests;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        //RegistrationTest.class,
-        Global_Search_Tests.class,
+        BulkOrderHeader_Tests.class,
+        Find_Equipment_Manuals_Search_Tests.class,
+        Global_Search_Tests.class
        //AddProductsToCartTest.class,
         //AddProductToMyPartsTest.class,
         //PlaceOrderTest.class
