5dfc19a495d12c86aae30cbc86923ff0e4eceb7b
wowkai <wowkaiwork@gmail.com>
2018-02-27 09:43:40 +0200

    Manufacturers count test

--- src/main/java/pages/ManufacturerLandingPage.java
+++ src/main/java/pages/ManufacturerLandingPage.java
@@ -14,6 +14,6 @@ public class ManufacturerLandingPage extends ParentPage {
     }
 
     public void clickOnManufactureLink(String manufacturerName) {
-        actionsWithOurWebElements.clickOnWebElement(".//*[@data-name='" + manufacturerName + "']");
+        actionsWithOurWebElements.clickOnWebElement(".//*[@data-name='" + manufacturerName + "'][@data-type='shop manufacturer']");
     }
 }
--- src/main/java/pages/SearchPage.java
+++ src/main/java/pages/SearchPage.java
@@ -4,13 +4,30 @@ package pages;
  * Search page is used to show listing of products which was returned after using site search feature
  */
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SearchPage extends ParentPage {
+    String countParts;
+    String countModels;
 
     public SearchPage(WebDriver webDriver) {
         super(webDriver);
     }
 
-    // method for check if searched keyword was applied as refinement
+    public String getPartsCount() {
+        countParts = "";
+        countParts = webDriver.findElement(By.cssSelector(".active.product-tabs-item")).getAttribute("data-parts-count");
+        //System.out.println(count);
+        return countParts;
+        }
+
+
+    public String getModelCount() {
+        countModels = "";
+        countModels = webDriver.findElement(By.xpath(".//ul[@class='product-tabs product-tabs--space']/li[2]")).getText();
+        return countModels;
+    }
 }
+
+    // method for check if searched keyword was applied as refinement
